a6e87fb6805b200a036cc7200b62f37cbe391315
Yuniski <yuniski24@gmail.com>
2023-05-17 13:13:22 -0300

    JS funtion completed

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log('holi')
-
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
@@ -14,7 +12,17 @@ const fetchData = async(id) => {
   try {
     const res = await fetch (`https://rickandmortyapi.com/api/character/${id}`)
     const data = await res.json()
-    cardView(data)
+
+    const character = {
+      img: data.image,
+      name: data.name,
+      status:data.status,
+      gender: data.gender,
+      origin: data.origin.name,
+      species: data.species,
+     }
+
+    cardView(character)
   } catch (error) {
     console.log(error)
   }
@@ -27,7 +35,13 @@ const cardView = (character) => {
   const clone = template.cloneNode(true);
   const fragment = document.createDocumentFragment();
 
-  clone.querySelector('.card-body-img').setAttribute('src' , character.image)
+  clone.querySelector('.card-body-img').setAttribute('src' , character.img);
+  clone.querySelector('.card-body-title').innerHTML = `${character.name}`;
+  clone.querySelector('.card-body-text').textContent = character.origin;
+  clone.querySelectorAll('.card-footer-social h3')[0].textContent = character.species;
+  clone.querySelectorAll('.card-footer-social h3')[1].textContent = character.gender;
+  clone.querySelectorAll('.card-footer-social h3')[2].textContent = character.status;
+
 
   fragment.appendChild(clone)
   flex.appendChild(fragment)
