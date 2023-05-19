599ac0c6de528f02c9cd7eaf8d7b314a84188713
Yuniski <yuniski24@gmail.com>
2023-05-18 21:49:08 -0300

    duplicated card

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  const radom = getRandomInt(1,201)
-  fetchData(radom)
+  const random = getRandomInt(1,201);
+  fetchData(random);
+  const random2 = getRandomInt(1,201);
+  fetchData(random2);
 })
 
 const fetchData = async(id) => {
   try {
-    const res = await fetch (`https://rickandmortyapi.com/api/character/${id}`)
-    const data = await res.json()
+    const res = await fetch (`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await res.json();
 
     const character = {
       img: data.image,
@@ -20,20 +21,19 @@ const fetchData = async(id) => {
       gender: data.gender,
       origin: data.origin.name,
       species: data.species,
-     }
+     };
 
-    cardView(character)
+    createCard(character);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-const cardView = (character) => {
-  console.log(character)
+const createCard = (character) => {
   const flex = document.querySelector('.flex');
-  const template = document.querySelector('#template-card').content;
-  const clone = template.cloneNode(true);
-  const fragment = document.createDocumentFragment();
+  const template = document.querySelector('#template-card');
+  const clone = template.content.cloneNode(true);
+  // const fragment = document.createDocumentFragment();
 
   clone.querySelector('.card-body-img').setAttribute('src' , character.img);
   clone.querySelector('.card-body-title').innerHTML = `${character.name}`;
@@ -42,7 +42,7 @@ const cardView = (character) => {
   clone.querySelectorAll('.card-footer-social h3')[1].textContent = character.gender;
   clone.querySelectorAll('.card-footer-social h3')[2].textContent = character.status;
 
-
-  fragment.appendChild(clone)
-  flex.appendChild(fragment)
+  // fragment.appendChild(clone)
+  // flex.appendChild(fragment)
+  flex.appendChild(clone);
 }
